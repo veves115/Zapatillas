5d9f58dce17d3c9501ccbec16f67cdd5d65cad24
veves115 <hoyesveves7@gmail.com>
2026-02-11 23:25:01 +0100

    Exception -> RuntimeException

--- src/main/java/es/pabloab/zapatillas/rest/zapatillas/exceptions/ZapatillaNotFoundException.java
+++ src/main/java/es/pabloab/zapatillas/rest/zapatillas/exceptions/ZapatillaNotFoundException.java
@@ -2,10 +2,10 @@ package es.pabloab.zapatillas.rest.zapatillas.exceptions;
 
 public class ZapatillaNotFoundException extends ZapatillaException {
     public ZapatillaNotFoundException(Long id) {
-        super("No se encontro la zapatill con id=" + id);
+        super("No se encontró la zapatilla con id=" + id);
     }
 
     public ZapatillaNotFoundException(String uuid) {
-        super("No se encontro la zapatill con uuid=" + uuid);
+        super("No se encontró la zapatilla con uuid=" + uuid);
     }
 }
